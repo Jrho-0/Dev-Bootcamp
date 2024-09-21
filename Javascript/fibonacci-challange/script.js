f90a4e7460374f832810edd63c4a1ec582465416
unknown <jarhodee@gmail.com>
2024-09-21 15:19:30 +0100

    fibonacci challange completed

---
+++ Javascript/fibonacci-challange/script.js
@@ -0,0 +1,15 @@
+function fibonacciGenerator(n) {
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    } else if (n === 2) {
+        output = [0, 1];
+    } else {
+        output = [0, 1]; //for n>2, new sequence start with [0, 1] automatically, using out.push, the new output is pushed into the constant array defined. 
+        for (i = 2; i < n; i++) {
+           var newOutput = output[output.length - 2] + output[output.length - 1];
+           output.push(newOutput);
+        }
+    }
+    return output
+}
