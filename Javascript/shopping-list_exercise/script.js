38acb66e1e332c20a7187bad36808d31f91292cb
unknown <jarhodee@gmail.com>
2024-09-30 03:36:57 +0100

    refactored code solution

--- Javascript/shopping-list_exercise/script.js
+++ Javascript/shopping-list_exercise/script.js
@@ -2,20 +2,27 @@ var button = document.querySelector("#btn");
 var input = document.querySelector("#userinput");
 var ul = document.querySelector("ul")
 
-button.addEventListener("click", function() {
-    if (input.value.length > 0 ) {
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
-    }
-})
+function inputLength() {
+    return input.value.length
+}
 
-input.addEventListener("keydown", function(event) {
-    if (input.value.length > 0 && event.key === "Enter") {
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";   
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0 ) {
+        createListElement()
     }
-})
+}
+
+function addListAfterKeydown(event) {
+    
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keydown", addListAfterKeydown)
