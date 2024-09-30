d2b12c07d49f1f402f3a380bd1930d7671462387
unknown <jarhodee@gmail.com>
2024-09-30 03:34:38 +0100

    exercise completed

--- Javascript/shopping-list_exercise/script.js
+++ Javascript/shopping-list_exercise/script.js
@@ -3,7 +3,19 @@ var input = document.querySelector("#userinput");
 var ul = document.querySelector("ul")
 
 button.addEventListener("click", function() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode("testing"));
-    ul.appendChild(li);
+    if (input.value.length > 0 ) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+    }
+})
+
+input.addEventListener("keydown", function(event) {
+    if (input.value.length > 0 && event.key === "Enter") {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";   
+    }
 })
