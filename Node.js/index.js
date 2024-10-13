13f389bae7d874dd3862da4485598dc8b1414d9d
unknown <jarhodee@gmail.com>
2024-10-13 15:26:40 +0100

    index.js updated

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,6 +1,17 @@
+//Write message to message.txt
+
 const fs = require("fs");
 
 fs.writeFile("message.txt", "Hello, Welcome to Node.js", (err) => {
     if (err) throw err;
     console.log("This file have been saved!")
+});
+
+//Read message from message.txt
+
+const fs = require("fs");
+
+fs.readFile("./message.txt", "utf8", (err,data) => {
+    if (err) throw err;
+    console.log(data);
 });
