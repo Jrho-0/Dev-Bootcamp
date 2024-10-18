70181ebd3f405afb2123d95b0564c200818381c7
unknown <jarhodee@gmail.com>
2024-10-18 13:58:12 +0100

    index.ejs modified

--- Backend/1.4.1 EJS Tags/index.js
+++ Backend/1.4.1 EJS Tags/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get("/", (req, res) => {
     const data = {
         title: "EJS Tags",
-        seconds: new Date().getSeconds,
+        seconds: new Date().getSeconds(),
         items: ["Orange", "Banana", "Cherry"],
         htmlContent: "<strong>This is some strong text</strong>"
     };
